34938d31c52e4593cc9c907fc0806fce95a62c7a
Sek <shinobu22@outlook.com>
2020-01-27 14:31:39 +0700

    [UPDATE] prepare login form

--- frontend/src/app/components/member/login/login.component.ts
+++ frontend/src/app/components/member/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  myApp: string = "Workshop POS";
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSayHi() {
+    // alert(this.myApp + this.onGetName());
+  }
+
+  onGetName(): string {
+    return "Pom";
+  }
+
+  login(loginForm: NgForm) {
+    alert(JSON.stringify(loginForm.value));
   }
 
 }
